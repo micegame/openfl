07835532eeecd0ce320fcfb898abfef448a503c8
Junsred <63877982+Junsred@users.noreply.github.com>
2022-05-05 21:05:50 +0300

    restrict set_scrollV

--- src/openfl/text/TextField.hx
+++ src/openfl/text/TextField.hx
@@ -2812,7 +2812,10 @@ class TextField extends InteractiveObject
 	{
 		__updateLayout();
 
-		if (value > 0 && value != __textEngine.scrollV || __textEngine.scrollV == 0)
+		if (value > __textEngine.maxScrollV) value = __textEngine.maxScrollV;
+		if (value < 1) value = 1;
+
+		if (value != __textEngine.scrollV || __textEngine.scrollV == 0)
 		{
 			__dirty = true;
 			__setRenderDirty();
